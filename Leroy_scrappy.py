6a1a1b84a47f08ba5b3706118c68a1f7ca45ab57
Savelev Alexey <76411160+Savexel@users.noreply.github.com>
2022-02-19 21:32:43 +0300

    Update Leroy_scrappy.py

--- Leroy_scrappy.py
+++ Leroy_scrappy.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 import pandas as pd
 
-Url = input('скопируйте ссылку на категорию товаров/// пример: https://leroymerlin.ru/catalogue/laminat/ ')
+Url = input('скопируйте сюда ссылку на категорию товаров// пример: https://leroymerlin.ru/catalogue/laminat/ ')
 Headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0',
            'accept': '*/*'}
 FILE = 'Leroy.csv'
